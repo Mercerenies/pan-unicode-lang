a1dba0d04ef44d4bcbf1f3b84665cfd7c6e67b16
Silvio Mayolo <mercerenies@comcast.net>
2022-02-04 21:31:05 -0500

    Testing framework installed

---
+++ src/util.test.ts
@@ -0,0 +1,18 @@
+
+import * as Util from './util.js';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+describe('util.ts', function() {
+
+  describe('assertNever', function() {
+    it('should raise an exception when called', function() {
+
+      // Function should not be called (as enforced by the type
+      // system), so if we force a call, it should throw.
+      expect(() => Util.assertNever(null as any as never)).to.throw();
+
+    });
+  });
+
+});
